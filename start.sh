4f30af00c6d72090bfe2622847bf9295e603b84d
Parik <25786988+parikmaan@users.noreply.github.com>
2017-11-24 21:43:21 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d --name docker-mongodb --auth --mount source=/home/dev/localDesk/mongoDB/data/db,target=/data/db docker-mongodb
+docker run -d --name docker-mongodb --mount source=/home/dev/localDesk/mongoDB/data/db,target=/data/db docker-mongodb --auth
