8cb896ad503985311fcf72acacc4bfcbe760003c
Parik <25786988+parikmaan@users.noreply.github.com>
2017-11-25 17:58:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,1 @@
-docker run --name docker-mongodb -it \
--v /data/db:/data/db \
--e MONGODB_ADMIN_USER=admin \
--e MONGODB_ADMIN_PASS=adminpass \
--e MONGODB_APPLICATION_DATABASE=mytestdatabase \
--e MONGODB_APPLICATION_USER=testuser \
--e MONGODB_APPLICATION_PASS=testpass \
--p 27017:27017 parikmaan/docker-mongodb:latest
+docker run -d --name docker-mongodb -v /data/db:/data/db parikmaan/docker-mongodb
