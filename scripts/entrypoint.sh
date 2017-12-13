25944fdd9f808a744c26d87e332d1342c17031e4
Parik <25786988+parikmaan@users.noreply.github.com>
2017-12-12 22:44:46 -0500

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -m
 
-/usr/bin/mongod --config /mongodb/conf/mongodb.conf
+mongodb_cmd="mongod --config /mongodb/conf/mongodb.conf"
+cmd="$mongodb_cmd"
+
+$cmd &
+
+if [ ! -f /mongodb/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
+
+fg
