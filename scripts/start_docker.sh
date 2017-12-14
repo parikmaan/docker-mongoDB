66d7de9ec3d7cf93d9bde867ef556addad20f73f
Parik <25786988+parikmaan@users.noreply.github.com>
2017-12-13 19:26:09 -0500

    Update start_docker.sh

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,1 +1,1 @@
-docker container run -d -p 27017:27017 --name docker-mongodb --mount type=bind, source=/mongodb,target=/mongodb parikmaan/docker-mongodb
+docker container run -d -p 27017:27017 parikmaan/docker-mongodb
